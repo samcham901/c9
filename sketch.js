3ca2efe724ecd5ff0ac4d046e194f3c5e95fb67e
samcham901 <87777327+samcham901@users.noreply.github.com>
2021-07-22 19:03:32 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,16 +1,37 @@
 var box_red;
 var box_green
+
+//var MouseEvent: {
+  //new (type: string, eventInitDict?: MouseEventInit): MouseEvent;
+  //prototype: MouseEvent;
+//}
 function setup() {
   createCanvas(400,400);
   
-  box_red = createSprite(100,200,50,30)
-  box_red = createSprite(100,200,50,30)
+  box_red = createButton("RED");
+  box_red.position(100,200)
+  box_red.mousePressed(red_press);
+  box_green = createButton("GREEN");
+  box_green.position(300,200)
+  box_green.mousePressed(green_press);
+  
 }
 
+
+function red_press(){
+  r=255
+  g=0
+  b=0
+}
+function green_press(){
+  r=0
+  g=255
+  b=0
+}
 function draw() 
 {
   background(50);
-//if(Mouse)
+if(MouseEvent)
 
   drawSprites();
 }
